ca1f469a74166fbd88326088b4ab9ac128fd8e2c
Stefan Zholtovski <stefan@spread.ai>
2022-09-01 13:41:11 +0300

    Added env var for port

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const { ApolloServer } = require('apollo-server');
+const {ApolloServer} = require('apollo-server');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
 async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => {
-      return {
-        trackAPI: new TrackAPI(),
-      };
-    },
-  });
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        dataSources: () => {
+            return {
+                trackAPI: new TrackAPI(),
+            };
+        },
+    });
 
-  const { url, port } = await server.listen();
-  console.log(`
+    const {url, port} = await server.listen({port: process.env.PORT || 4000});
+    console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
       📭  Query at ${url}
